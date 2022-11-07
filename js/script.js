2b5cad714c119b8de92f1412982f24b05f8d1831
RobsonAndrade1 <andrade.rob1691@gmail.com>
2022-11-06 22:30:55 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
 const score = document.querySelector(".score");
 let count = 0;
 
@@ -16,7 +17,6 @@ const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
-    
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) { 
 
@@ -25,12 +25,13 @@ const loop = setInterval(() => {
 
         mario.style.animation = "none"
         mario.style.bottom = `${marioPosition}px`;
-
+        
         mario.src = "./imagens/game-over.png";
         mario.style.width = "75px";
         mario.style.marginLeft = "50px";
 
         clearInterval(loop);
+
     }
 
     count++;
